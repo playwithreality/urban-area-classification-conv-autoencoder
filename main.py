f6134827865fba8c5f9afa369fb69a6ba0cbec64
samniem <sami.nieminen.hw@gmail.com>
2021-03-22 17:08:31 +0200

    Average Pooling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import AveragePooling2D
 
 #We expect tensorflow >2.3.2, preferably 2.4 or greater
 print(tf.__version__)
@@ -17,7 +18,10 @@ input_shape = (100,100,2)
 inputs = Input(shape=input_shape)
 #missing transformations
 conv = Conv2D(filters=32, kernel_size=3, activation='relu')(inputs)
-model = Model(inputs=inputs, outputs=conv)
+#pool size was not specified we an probably use 2x2 or 3x3 pooling
+pooling = AveragePooling2D(pool_size=(3,3))(conv)
+
+model = Model(inputs=inputs, outputs=pooling)
 
 print(model.summary())
 ##end of summary should be (None, 1) in order to make model work
