a3ab2759f88a7509c80b8d8dad455f025e721fe5
samniem <sami.nieminen.hw@gmail.com>
2021-03-22 11:36:51 +0200

    Update @Jessuuuu work to Main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import dataloader as d
 import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
 
 #We expect tensorflow >2.3.2, preferably 2.4 or greater
 print(tf.__version__)
@@ -7,3 +12,20 @@ print(tf.__version__)
 #train_ds, validation_ds = d.rgb_loader()
 #train_ds, val_ds = d.calib_loader()
 train_x, train_y, test_x, test_y = d.get_manual_calib_data()
+
+input_shape = (100,100,2)
+inputs = Input(shape=input_shape)
+#missing transformations
+conv = Conv2D(filters=32, kernel_size=3, activation='relu')(inputs)
+model = Model(inputs=inputs, outputs=conv)
+
+print(model.summary())
+##end of summary should be (None, 1) in order to make model work
+
+#model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+#model.fit(x=train_x, y=train_y, epochs=1)
+
+
+#model = models.Sequential()
+#model.add(Conv2D(filters=32, kernel_size=3, activation='relu'))
+#model.complie()
