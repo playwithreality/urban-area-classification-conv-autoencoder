4a1f50261f1b02c9f7398d3c1dee66515a9aaea2
jesper suojoki <jesper.suojoki@aalto.fi>
2021-03-24 05:31:56 +0200

    added basic autoencoders with dropouts

--- main.py
+++ main.py
@@ -16,12 +16,24 @@ train_x, train_y, test_x, test_y = d.get_manual_calib_data()
 
 input_shape = (100,100,2)
 inputs = Input(shape=input_shape)
-#missing transformations
-conv = Conv2D(filters=32, kernel_size=3, activation='relu')(inputs)
+
+#convolutional layer
+conv = Conv2D(32, kernel_size=3, activation='relu')(inputs)
 #pool size was not specified we an probably use 2x2 or 3x3 pooling
 pooling = AveragePooling2D(pool_size=(3,3))(conv)
-
-model = Model(inputs=inputs, outputs=pooling)
+#first auto encoder
+encoded1 = Dense(16, activation='relu', #non-linear activation
+                activity_regularizer=regularizers.l1(10e-5))(pooling)#more sparse than 2nd autoencoder
+decoded1 = Dense(32, activation='softmax')(encoded1)#softmax applied
+dropout1 = Dropout((0.2))(decoded1)#dropout with 0.2 rate
+#second auto encoder
+encoded2 = Dense(8, activation='relu') (dropout1)#non-linear activation
+decoded2 = Dense(16, activation='softmax')(encoded2)#softmax applied
+dropout2 = Dropout((0.2))(decoded2)
+#classification
+flatten = Flatten()(dropout2)
+output = Dense(1, activation='sigmoid')(flatten)
+model = Model(inputs=inputs, outputs=output)
 
 print(model.summary())
 ##end of summary should be (None, 1) in order to make model work
